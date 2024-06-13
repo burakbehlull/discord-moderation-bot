9cdf8f17a21e9bc089bfeb7eac85d559e9f55f94
burakbehlull <burakyabgu@gmail.com>
2024-06-13 20:51:16 +0300

    feat: Complete configuration of commands folder

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js')
+const path = require('path')
+const fs = require('fs')
 require('dotenv').config()
 
 var client = new Client({
@@ -22,11 +24,28 @@ var client = new Client({
     ],
 })
 
+client.commands = new Collection()
+
+// Komutlar klasörü (commands file)
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[HATA] ${filePath} içinde 'data' ve 'execute' yok.`)
+	}
+}
+console.log(`${commandFiles.length} komut yüklendi.`);
+
 
 client.on('ready', () => {
     console.log('Bot hazır!')
 })
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
 .then(()=>console.log('Token girişi başarılı'))
 .catch((err)=>console.log("Hata: ", err))
