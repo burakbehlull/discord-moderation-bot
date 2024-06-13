86cf94411051170203115f71e420773a11dac95f
burakbehlull <burakyabgu@gmail.com>
2024-06-13 20:56:35 +0300

    feat: Create deploy-commands file

---
+++ deploy-commands.js
@@ -0,0 +1,33 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
+
+for (const commandFile of commandFiles) {
+	const command = require(path.join(foldersPath, commandFile));
+	if ('data' in command && 'execute' in command) {
+		commands.push(command.data.toJSON());
+	} else {
+		console.log(`[WARNING] The command at ${commandFile} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationCommands(process.env.BOT_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
