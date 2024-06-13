4e7447edea167a4bfb018b207a1709fcfeb27b2d
burakbehlull <burakyabgu@gmail.com>
2024-06-13 20:46:04 +0300

    chore: Set up Bot

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+
+
+var client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildBans, 
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations, 
+        GatewayIntentBits.GuildWebhooks, 
+        GatewayIntentBits.GuildInvites, 
+        GatewayIntentBits.GuildVoiceStates, 
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping, 
+        GatewayIntentBits.DirectMessages, 
+        GatewayIntentBits.DirectMessageReactions, 
+        GatewayIntentBits.DirectMessageTyping, 
+        GatewayIntentBits.MessageContent,
+    ],
+})
+
+
+client.on('ready', () => {
+    console.log('Bot hazır!')
+})
+
+client.login(TOKEN)
+.then(()=>console.log('Token girişi başarılı'))
+.catch((err)=>console.log("Hata: ", err))
