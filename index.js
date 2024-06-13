41570889a37d72990dc9ea60c6a0fab0b79e4ec0
burakbehlull <burakyabgu@gmail.com>
2024-06-13 20:52:35 +0300

    feat: Complete configuration of events  folder

--- index.js
+++ index.js
@@ -41,6 +41,19 @@ for (const file of commandFiles) {
 }
 console.log(`${commandFiles.length} komut yüklendi.`);
 
+// Durum klasörü (events file)
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, ActivityType));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
 client.on('ready', () => {
     console.log('Bot hazır!')
