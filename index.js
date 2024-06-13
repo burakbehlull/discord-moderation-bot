141fe1a5083e24a7367f3483b5a8d8c18acc916f
burakbehlull <burakyabgu@gmail.com>
2024-06-13 20:54:44 +0300

    feat: Create interactionCreate event

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ var client = new Client({
 client.commands = new Collection()
 
 // Komutlar klasörü (commands file)
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
 	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
+		client.commands.set(command.data.name, command)
 	} else {
 		console.log(`[HATA] ${filePath} içinde 'data' ve 'execute' yok.`)
 	}
 }
-console.log(`${commandFiles.length} komut yüklendi.`);
+console.log(`${commandFiles.length} komut yüklendi.`)
 
 // Durum klasörü (events file)
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
+	const filePath = path.join(eventsPath, file)
+	const event = require(filePath)
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, ActivityType));
+		client.once(event.name, (...args) => event.execute(...args, ActivityType))
 	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
+		client.on(event.name, (...args) => event.execute(...args, client))
 	}
 }
 
