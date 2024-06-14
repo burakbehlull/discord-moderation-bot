2a4f4b29b553c6941f7bbccf46a99b053ab2bc1a
burakbehlull <burakyabgu@gmail.com>
2024-06-14 12:12:08 +0300

    feat: Check for booster status

--- commands/boosterSpecial.js
+++ commands/boosterSpecial.js
@@ -167,69 +167,75 @@ module.exports = {
 
 client.on("interactionCreate", async (interaction) => {
     if(interaction.isModalSubmit()){
-        if (interaction.customId === "addmodal") {
-            const rolName = interaction.fields.getTextInputValue('rolname')
-            const color = interaction.fields.getTextInputValue('color')
-            const emojiId = interaction.fields.getTextInputValue('emojiId')
-            const hashtag = color.startsWith('#')
-            if(!hashtag) return await interaction.reply('Renk ataması yaparken lütfen başına # koyup, renk kodunuz giriniz.')
-            
-            const user = await User.findOne({userID: interaction.user.id})
-            
-            if(user?.limit) return await interaction.reply('Rolünüz zaten var!')
-            const role = await interaction.guild.roles.create({
-                name: rolName,
-                color: color,
-                icon: `https://cdn.discordapp.com/emojis/${emojiId}.png?size=96&quality=lossless`
-            })
-            if(user){
-                user.role = interaction.user.id
-                user.limit = false
+        const member = await interaction.guild.members.fetch(interaction.user.id)
+        const isBooster = member.roles.cache.some(role => role.tags && role.tags.premiumSubscriberRole)
+        if(isBooster){
+            if (interaction.customId === "addmodal") {
+                const rolName = interaction.fields.getTextInputValue('rolname')
+                const color = interaction.fields.getTextInputValue('color')
+                const emojiId = interaction.fields.getTextInputValue('emojiId')
+                const hashtag = color.startsWith('#')
+                if(!hashtag) return await interaction.reply('Renk ataması yaparken lütfen başına # koyup, renk kodunuz giriniz.')
+                
+                const user = await User.findOne({userID: interaction.user.id})
+                
+                if(user?.limit) return await interaction.reply('Rolünüz zaten var!')
+                const role = await interaction.guild.roles.create({
+                    name: rolName,
+                    color: color,
+                    icon: `https://cdn.discordapp.com/emojis/${emojiId}.png?size=96&quality=lossless`
+                })
+                if(user){
+                    user.role = interaction.user.id
+                    user.limit = false
+                    user.eId = emojiId
+                    await user.save()
+                } else {
+                    await User.create({
+                        userID: interaction.user.id,
+                        role: role.id,
+                        limit: true,
+                        eId: emojiId
+                    })
+                }
+                return await interaction.reply('Rol oluşturuldu.')
+            }
+            if (interaction.customId === "editmodal") {
+                const rolName = interaction.fields.getTextInputValue('rolname')
+                const color = interaction.fields.getTextInputValue('color')
+                const emojiId = interaction.fields.getTextInputValue('emojiId')
+    
+                const user = await User.findOne({userID: interaction.user.id})
+                const uRole = interaction.guild.roles.cache.get(user.role)
+                const convertColor = color.startsWith('#') ? color : Number(color)
+                
+                await uRole.edit({
+                    name: rolName,
+                    color: convertColor,
+                    icon: `https://cdn.discordapp.com/emojis/${emojiId}.png?size=96&quality=lossless`
+                })
                 user.eId = emojiId
                 await user.save()
-            } else {
-                await User.create({
-                    userID: interaction.user.id,
-                    role: role.id,
-                    limit: true,
-                    eId: emojiId
-                })
+                return await interaction.reply('Rol düzenlendi.')
             }
-            return await interaction.reply('Rol oluşturuldu.')
-        }
-        if (interaction.customId === "editmodal") {
-            const rolName = interaction.fields.getTextInputValue('rolname')
-            const color = interaction.fields.getTextInputValue('color')
-            const emojiId = interaction.fields.getTextInputValue('emojiId')
-
-            const user = await User.findOne({userID: interaction.user.id})
-            const uRole = interaction.guild.roles.cache.get(user.role)
-            const convertColor = color.startsWith('#') ? color : Number(color)
-            
-            await uRole.edit({
-                name: rolName,
-                color: convertColor,
-                icon: `https://cdn.discordapp.com/emojis/${emojiId}.png?size=96&quality=lossless`
-            })
-            user.eId = emojiId
-            await user.save()
-            await interaction.reply('Rol düzenlendi.')
-        }
-        if(interaction.customId === "useraddModal") {
-            const userId = interaction.fields.getTextInputValue('chosenUserId')
-            const user = await User.findOne({userID: interaction.user.id})
-            const uRole = interaction.guild.roles.cache.get(user.role)
-            const member = await interaction.guild.members.fetch(userId)
-
-            if(!member) return await interaction.reply('Kullanıcı bulunamadı')
-            if(!uRole) return await interaction.reply('Rol bulunamadı')
-            if(user?.hasRole.includes(userId)) return await interaction.reply('Kullanıcı zaten role sahip.')
-            
-            await member.roles.add(user.role)
-            await user.hasRole.push(userId)
-            await user.save()
-            await interaction.reply(`<@${userId}> başarıyla role eklendi.`)
-        }
-        return;
+            if(interaction.customId === "useraddModal") {
+                const userId = interaction.fields.getTextInputValue('chosenUserId')
+                const user = await User.findOne({userID: interaction.user.id})
+                const uRole = interaction.guild.roles.cache.get(user.role)
+                const member = await interaction.guild.members.fetch(userId)
+    
+                if(!member) return await interaction.reply('Kullanıcı bulunamadı')
+                if(!uRole) return await interaction.reply('Rol bulunamadı')
+                if(user?.hasRole.includes(userId)) return await interaction.reply('Kullanıcı zaten role sahip.')
+                
+                await member.roles.add(user.role)
+                await user.hasRole.push(userId)
+                await user.save()
+                return await interaction.reply(`<@${userId}> başarıyla role eklendi.`)
+            }
+            return;
+        } else {
+			return await interaction.reply('Bu komutu sadece Boosterlar kullanabilir.')
+		}
     }
 })
